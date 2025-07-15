f8dd4aa69c2dec9a62258647469441090b632581
Albert <Izuku@DESKTOP-125A3DF>
2025-07-15 23:08:55 +0500

    SwitchBackground with transition DONE

--- 02-JavaScript/02-functions/functions.js
+++ 02-JavaScript/02-functions/functions.js
@@ -9,16 +9,9 @@ function Power(base, exponent) {
 }
 function SwitchBackground() {
 	let switchButton = document.getElementById('switchBackground');
-	console.log(switchButton.attributes.src);
-	if (switchButton.attributes.src.nodeValue == 'img/moon.png') {
-		switchButton.attributes.src.nodeValue = 'img/sun.png';
-		document.body.style.backgroundColor = "#323232";
-		document.body.style.color = "white";
-	}
-	else {
-		switchButton.attributes.src.nodeValue = 'img/moon.png';
-		document.body.style.backgroundColor = "#FFFFFF";
-		document.body.style.color = "black";
-	}
-
+	let delay = Number(document.getElementById("delay").value);
+	console.log(delay);
+	document.body.style.transition = `background-color ${delay}s, color ${delay}s`;
+	document.getElementById('switchBackground').style.transition = `background-image ${delay}s`;
+	document.body.className = document.body.className === "light" ? "dark" : "light";
 }
