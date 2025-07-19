13c696f4fe044f8288844d16264d8bc33d0ea4f7
Albert <Izuku@DESKTOP-125A3DF>
2025-07-19 15:34:16 +0500

    SetImage DONE Lada

--- 02-JavaScript/02-functions/functions.js
+++ 02-JavaScript/02-functions/functions.js
@@ -15,3 +15,40 @@ function SwitchBackground() {
 	document.getElementById('switchBackground').style.transition = `background-image ${delay}s`;
 	document.body.className = document.body.className === "light" ? "dark" : "light";
 }
+document.addEventListener("mousemove", function (event) {
+	let x = event.clientX;
+	let y = event.clientY;
+	document.getElementById("mouse").innerHTML = `X = ${x}, Y = ${y}`;
+}
+);
+
+function setImage() {
+	let filename = document.getElementById("image-file");
+	let reader = new FileReader();
+	reader.onload = function (e) {
+		document.getElementById("photo").src = e.target.result;
+	}
+	reader.readAsDataURL(filename.files[0]);
+}
+
+document.body.onload = function tick_timer() {
+	let time = new Date();
+	document.getElementById("full-time").innerHTML = time;
+	document.getElementById("hours").innerHTML = addLeadingZero(time.getHours());
+	document.getElementById("minutes").innerHTML = addLeadingZero(time.getMinutes());
+	document.getElementById("seconds").innerHTML = addLeadingZero(time.getSeconds());
+
+	document.getElementById("year").innerHTML = time.getFullYear();
+	document.getElementById("month").innerHTML = addLeadingZero(time.getMonth() + 1);
+	document.getElementById("day").innerHTML = addLeadingZero(time.getDate());
+
+	document.getElementById("weekday").innerHTML = time.toLocaleDateString("ru", { weekday: 'long' });
+
+	document.getElementById("current-date").style.visibility = document.getElementById("show-date").checked ? "visible" : "hidden";
+	document.getElementById("weekday").style.visibility = document.getElementById("show-weekday").checked ? "visible" : "hidden";
+
+	setTimeout(tick_timer, 100);
+}
+function addLeadingZero(number) {
+	return number < 10 ? "0" + number : number;
+}
